69a2257787a5096c39d182681480890a48171e38
github username <github email address>
2020-06-03 11:45:23 -0700

    Added serializers for services and started service controller

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,18 @@
 class Service < ApplicationRecord
     belongs_to :church
     has_many :service_sections
+
+    def self.serialized_services
+        Service.all.map {|service| {date: service.date, id: service.id}} #should encrypt this in the future
+    end
+
+    def serialized_sections
+        self.service_sections.map {|service_section| {
+            title: service_section.title, 
+            id: service_section.id,
+            slides: service_section.slides.map {|slide| {title: slide.title ? slide.title : service_section.title, content: slide.content, id: slide.id}}.sort{|slide| -slide[:id] }
+            }
+        }.sort{|service_section| -service_section[:id] }
+    end
+
 end
