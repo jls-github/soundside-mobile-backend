4aa62da4407ae853d45ee0084d3ee52031607b0b
jls-github <souzajohnlee@gmail.com>
2020-06-03 11:45:23 -0700

    Added serializers for services and started service controller

---
+++ app/controllers/service_controller.rb
@@ -0,0 +1,18 @@
+class ServiceController < ApplicationController
+
+    def index
+        render json: Services.serialized_services
+    end
+
+    def show
+        service = Service.find(params[:id])
+        if service
+            service_sections = service.serialized_sections
+            render json: service_sections
+        else
+            render json: {error: "Couldn't find the service you were looking for"}
+        end
+    end
+
+
+end
