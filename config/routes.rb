5fe47ee0bb9edaef02c3f2b3bc25a1df4b8fef50
github username <github email address>
2020-06-05 12:12:41 -0700

    Changed create from patch to put

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :services, only: [:index, :show, :update, create]
+  resources :services, only: [:index, :show, :create]
+  put '/services/:id', to: 'services#update'
 
 end
