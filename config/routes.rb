275b2c22ab26132caace7b13624264cf4422fcfc
jls-github <souzajohnlee@gmail.com>
2020-06-05 12:13:09 -0700

    Added in delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :services, only: [:index, :show, :create]
+  resources :services, only: [:index, :show, :create, :delete]
   put '/services/:id', to: 'services#update'
 
 end
