cf608bb94799bcc0eeecfcc23acb5c49d7d94ac1
github username <github email address>
2020-06-11 16:42:41 -0700

    removed before_save and added title in serializer

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,10 +1,4 @@
 class Slide < ApplicationRecord
   belongs_to :service_section
   validates :content, presence: true
-  before_save :default_values
-
-
-  def default_values
-    self.title = self.section.title if self.title.nil?
-  end
 end
