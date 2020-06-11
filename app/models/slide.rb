6b72d3aa9ce2e9962ba57fcf448807d43382187c
jls-github <souzajohnlee@gmail.com>
2020-06-11 16:37:13 -0700

    Slides now input their section's title if nil

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,4 +1,10 @@
 class Slide < ApplicationRecord
   belongs_to :service_section
   validates :content, presence: true
+  before_save: default_values
+
+
+  def default_values
+    self.title = self.section.title if self.title.nil?
+  end
 end
