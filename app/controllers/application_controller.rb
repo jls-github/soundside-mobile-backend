82503722723c6227e0c910f9d08221334a36e462
jls-github <souzajohnlee@gmail.com>
2020-06-11 12:39:34 -0700

    jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
     before_action :authorized
-    JWT_SECRET = Rails.application.credentials.secret_key_base
+    JWT_SECRET = Rails.application.credentials.dig(:secret_key_base)
 
     def encode_token(payload)
-        JWT.encode(payload, "JWT_SECRET")
+        JWT.encode(payload, JWT_SECRET)
     end
 
     def auth_header
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(" ")[1]
             begin
-                JWT.decode(token, "JWT_SECRET")
+                JWT.decode(token, JWT_SECRET)
             rescue JWT::DecodeError
                 nil
             end
