9a3fe87ebfbbc4a4830629b86cee31b7ec4891f9
github username <github email address>
2020-06-17 13:39:30 -0700

    changed date to created_at

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,14 +1,14 @@
 class Connection < ApplicationRecord
 
     def self.serialized_connections
-        Connection.order(date: :desc).map{|connection| {
+        Connection.order(created_at: :desc).map{|connection| {
             name: connection.name, 
             email: connection.email, 
             comment: connection.comment, 
             learn_about_jesus: connection.learn_about_jesus, 
             hear_about_church: connection.hear_about_church, 
             talk_to_pastor: connection.talk_to_pastor, 
-            date: connection.date.strftime("%Y-%m-%d")
+            date: connection.created_at.strftime("%Y-%m-%d")
         }}
     end
     
