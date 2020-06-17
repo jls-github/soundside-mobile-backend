7e4fbb32664e5d7c33d5471337f3461d91bd8dd1
jls-github <souzajohnlee@gmail.com>
2020-06-17 13:13:20 -0700

    Added connection card controller, model, migration

---
+++ app/controllers/connections_controller.rb
@@ -0,0 +1,21 @@
+class ConnectionController < ApplicationController
+    skip_before_action :authorized, only: :create
+    def index
+        render json: Connection.all
+    end
+
+    def create
+        connection = Connection.new(connection_params)
+        if connection.save
+            render json: {success: "connection successfully saved"}
+        else
+            render json: {error: "cannot save connection"}
+        end
+    end
+
+    private
+
+    def connection_params
+        params.require(:connection).permit(:name, :email, :comment, :learn_about_jesus, :hear_about_church, :talk_to_pastor)
+    end
+end
